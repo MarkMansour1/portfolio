7b1b21c2effe316434aa49882cd2d0530356276e
MarkMansour1 <markmansour01@gmail.com>
2022-03-24 19:11:45 +0000

    Home page

--- components/Layout.js
+++ components/Layout.js
@@ -4,18 +4,12 @@ import classNames from "util/classNames";
 
 import { motion } from "framer-motion";
 
-export default function Layout(props) {
+export default function Layout({ dark, children }) {
 	return (
-		<>
-			<motion.div
-				initial="initial"
-				animate="animate"
-				exit={{ opacity: 0 }}>
-				<Nav dark={!props.lightHeader} />
-				<main className="min-h-screen">{props.children}</main>
-				<Footer dark={!props.lightFooter} />
-			</motion.div>
-			{!props.noNoise && <div className="noise bg-gray-900" />}
-		</>
+		<motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
+			<Nav dark={dark} />
+			<main>{children}</main>
+			<Footer />
+		</motion.div>
 	);
 }
--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,78 @@ import Image from "next/image";
 import Link from "next/link";
 import Layout from "components/Layout";
 
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { fadeIn, fadeUp, stagger } from "util/animations";
 
+const projects = [
+	{ title: "The Cabin Studios", name: "cabinstudios" },
+	{ title: "CrepPlug", name: "crepplug" },
+	{ title: "Cemester", name: "cemester" },
+	{ title: "DM Fitness & Performance", name: "dmfnp" },
+	{ title: "Koto", name: "koto" },
+	{ title: "Firth Gardens", name: "firthgardens" },
+	{ title: "Denis Gardiner", name: "denisgardiner" },
+	{ title: "Kingdom Compassion", name: "kingdomcompassion" },
+	{ title: "Coco Cares", name: "cococares" },
+];
+
 export default function Index() {
+	const [selected, setSelected] = useState(0);
+	const [visible, setVisible] = useState(false);
+	const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+	let size = 400;
+	let half = size / 2;
+
 	return (
 		<Layout>
-			<div className="container mx-auto h-screen text-white text-center">
+			<motion.div
+				className="cursor"
+				style={{
+					width: size,
+					height: size,
+					opacity: visible ? 1 : 0,
+					transform: visible ? "scale(1)" : "scale(0)",
+					left: coords.x,
+					top: coords.y,
+				}}>
+				<Image
+					src={`/thumbnails/${projects[selected].name}.png`}
+					alt={projects[selected].title}
+					layout="fill"
+					objectFit="cover"
+				/>
+			</motion.div>
+			<div
+				className="max-w-5xl mx-auto"
+				onMouseMove={(e) => {
+					setCoords({
+						x: e.clientX - half,
+						y: e.clientY - half,
+					});
+				}}>
 				<motion.div
 					variants={stagger}
-					className="relative top-1/2 -translate-y-1/2 w-1/2 inline-block">
-					<motion.h1
-						variants={fadeUp}
-						className="text-5xl font-bold leading-tight mb-4">
-						Personal Training Reinvented
-					</motion.h1>
-					<motion.h3
-						variants={fadeUp}
-						className="text-3xl leading-snug mb-8">
-						Start your free trial today.
-						<br />
-						No credit card required.
-					</motion.h3>
-					<motion.div variants={fadeUp}>
-						<Link href="/">
-							<a className="px-8 py-4 border text-xs font-semibold uppercase bg-transparent transition-colors border-white text-white hover:bg-white hover:text-black">
-								Get Started →
-							</a>
-						</Link>
-					</motion.div>
+					className="divide-y divide-gray-300">
+					{projects.map((project, index) => (
+						<motion.div
+							variants={fadeUp}
+							key={project.name}
+							className="py-24 text-center">
+							<Link href={`/${project.name}`}>
+								<a
+									className="text-4xl hover:text-white hover:drop-shadow transition"
+									onMouseEnter={() => {
+										setVisible(true);
+										setSelected(index);
+									}}
+									onMouseLeave={() => setVisible(false)}>
+									{project.title}
+								</a>
+							</Link>
+						</motion.div>
+					))}
 				</motion.div>
 			</div>
 		</Layout>
